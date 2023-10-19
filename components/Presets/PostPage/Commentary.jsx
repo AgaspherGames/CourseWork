b5c34388c39ca06dd58a333dea995775abcccb3b
AgaspherGames <agasphergg@gmail.com>
2023-10-19 19:36:15 +0600

    commentary component

---
+++ components/Presets/PostPage/Commentary.jsx
@@ -0,0 +1,47 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ShadowView from '../../UI/Base/ShadowView'
+import { Image } from 'react-native'
+
+export default function Commentary() {
+  return (
+    <ShadowView classname="bg-white mx-4 p-4 rounded-xl flex-1 mb-4">
+        <View className="">
+          <View className="my-2 flex-row items-center">
+            <Image
+              source={{
+                uri: "https://th.bing.com/th/id/R.8112410131653a63c0596a57ebc85519?rik=TrmOhl0eZJU0Nw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-rL0UdLNivjY%2fUhvtGHddwUI%2fAAAAAAAAAy8%2fGPJ0ojd6G2w%2fs1600%2fpromotional-photoshoot-tyler-durden.jpg&ehk=t9CBGtalAmIr39aULbo2gDn5oZRATnhUic1bKpqCtto%3d&risl=&pid=ImgRaw&r=0",
+              }}
+              resizeMode="cover"
+              style={{
+                height: 48,
+                width: 48,
+              }}
+              className="rounded-full bg-gray-200"
+            />
+            <View className="ml-2 flex-col justify-end">
+              <Text
+                style={{ lineHeight: 20 }}
+                className="text-xl leading-tight font-medium mt-1"
+              >
+                Артем К.
+              </Text>
+              <Text
+                style={{ lineHeight: 20 }}
+                className="text-base text-gray-700 leading-tight "
+              >
+                @agaspher
+              </Text>
+            </View>
+          </View>
+          <Text style={{ lineHeight: 20 }} className="text-lg ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem libero
+            et accusantium perspiciatis tempora placeat, dicta cupiditate sequi
+            dolorem ducimus vero a beatae omnis optio obcaecati ea assumenda?
+            Ipsa ea repellendus repudiandae aperiam ducimus velit provident vero
+            nisi, enim cupiditate temp
+          </Text>
+        </View>
+      </ShadowView>
+  )
+}
--- pages/PostPage.jsx
+++ pages/PostPage.jsx
@@ -9,6 +9,7 @@ import { Pressable } from "react-native";
 import PostInfo from "../components/Presets/PostPage/PostInfo";
 import { TextInput } from "react-native";
 import SendButton from "../components/UI/buttons/SendButton";
+import Commentary from "../components/Presets/PostPage/Commentary";
 
 const styles = StyleSheet.create({
   wrapper: {},
@@ -108,129 +109,9 @@ export default function PostPage() {
         </ShadowView>
       </View>
 
-      <ShadowView classname="bg-white mx-4 p-4 rounded-xl flex-1 mb-4">
-        <View className="">
-          <View className="my-2 flex-row items-center">
-            <Image
-              source={{
-                uri: "https://th.bing.com/th/id/R.8112410131653a63c0596a57ebc85519?rik=TrmOhl0eZJU0Nw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-rL0UdLNivjY%2fUhvtGHddwUI%2fAAAAAAAAAy8%2fGPJ0ojd6G2w%2fs1600%2fpromotional-photoshoot-tyler-durden.jpg&ehk=t9CBGtalAmIr39aULbo2gDn5oZRATnhUic1bKpqCtto%3d&risl=&pid=ImgRaw&r=0",
-              }}
-              resizeMode="cover"
-              style={{
-                height: 48,
-                width: 48,
-              }}
-              className="rounded-full bg-gray-200"
-            />
-            <View className="ml-2 flex-col justify-end">
-              <Text
-                style={{ lineHeight: 20 }}
-                className="text-xl leading-tight font-medium mt-1"
-              >
-                Артем К.
-              </Text>
-              <Text
-                style={{ lineHeight: 20 }}
-                className="text-base text-gray-700 leading-tight "
-              >
-                @agaspher
-              </Text>
-            </View>
-          </View>
-          <Text style={{ lineHeight: 20 }} className="text-lg ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem libero
-            et accusantium perspiciatis tempora placeat, dicta cupiditate sequi
-            dolorem ducimus vero a beatae omnis optio obcaecati ea assumenda?
-            Ipsa ea repellendus repudiandae aperiam ducimus velit provident vero
-            nisi, enim cupiditate temp
-          </Text>
-        </View>
-      </ShadowView>
-      <ShadowView classname="bg-white mx-4 p-4 rounded-xl flex-1 mb-4">
-        <View className="">
-          <View className="my-2 flex-row items-center">
-            <Image
-              source={{
-                uri: "https://th.bing.com/th/id/R.8112410131653a63c0596a57ebc85519?rik=TrmOhl0eZJU0Nw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-rL0UdLNivjY%2fUhvtGHddwUI%2fAAAAAAAAAy8%2fGPJ0ojd6G2w%2fs1600%2fpromotional-photoshoot-tyler-durden.jpg&ehk=t9CBGtalAmIr39aULbo2gDn5oZRATnhUic1bKpqCtto%3d&risl=&pid=ImgRaw&r=0",
-              }}
-              resizeMode="cover"
-              style={{
-                height: 48,
-                width: 48,
-              }}
-              className="rounded-full bg-gray-200"
-            />
-            <View className="ml-2 flex-col justify-end">
-              <Text
-                style={{ lineHeight: 20 }}
-                className="text-xl leading-tight font-medium mt-1"
-              >
-                Артем К.
-              </Text>
-              <Text
-                style={{ lineHeight: 20 }}
-                className="text-base text-gray-700 leading-tight "
-              >
-                @agaspher
-              </Text>
-            </View>
-          </View>
-          <Text style={{ lineHeight: 20 }} className="text-lg ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem libero
-          </Text>
-        </View>
-      </ShadowView>
-      <ShadowView classname="bg-white mx-4 p-4 rounded-xl flex-1 mb-4">
-        <View className="">
-          <View className="my-2 flex-row items-center">
-            <Image
-              source={{
-                uri: "https://th.bing.com/th/id/R.8112410131653a63c0596a57ebc85519?rik=TrmOhl0eZJU0Nw&riu=http%3a%2f%2f1.bp.blogspot.com%2f-rL0UdLNivjY%2fUhvtGHddwUI%2fAAAAAAAAAy8%2fGPJ0ojd6G2w%2fs1600%2fpromotional-photoshoot-tyler-durden.jpg&ehk=t9CBGtalAmIr39aULbo2gDn5oZRATnhUic1bKpqCtto%3d&risl=&pid=ImgRaw&r=0",
-              }}
-              resizeMode="cover"
-              style={{
-                height: 48,
-                width: 48,
-              }}
-              className="rounded-full bg-gray-200"
-            />
-            <View className="ml-2 flex-col justify-end">
-              <Text
-                style={{ lineHeight: 20 }}
-                className="text-xl leading-tight font-medium mt-1"
-              >
-                Артем К.
-              </Text>
-              <Text
-                style={{ lineHeight: 20 }}
-                className="text-base text-gray-700 leading-tight "
-              >
-                @agaspher
-              </Text>
-            </View>
-          </View>
-          <Text style={{ lineHeight: 20 }} className="text-lg ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem libero
-            et accusantium perspiciatis tempora placeat, dicta cupiditate sequi
-            dolorem ducimus vero a beatae omnis optio obcaecati ea assumenda?
-            Ipsa ea repellendus repudiandae aperiam ducimus velit provident vero
-            nisi, enim cupiditate temporibus sunt earum nulla quia impedit
-            explicabo, nobis, soluta veniam? Lorem ipsum, dolor sit amet
-            consectetur adipisicing elit. Ducimus expedita cupiditate dolor
-            aperiam dolorum quos, odio commodi quisquam, iusto minima, quibusdam
-            eveniet. Autem tempora iusto aut odit rem quaerat sunt cum.
-            Obcaecati repellat quod quas incidunt quaerat quis placeat
-            blanditiis ratione in possimus, dicta assumenda dolores voluptates
-            totam est voluptate eaque eum quisquam nulla, cupiditate odit
-            impedit facere at. Id minima laudantium vitae maiores cupiditate
-            aspernatur? Ratione pariatur delectus nisi deserunt recusandae
-            quibusdam maxime suscipit nam voluptates deleniti. Repudiandae
-            minima quaerat eveniet architecto consequuntur harum quis itaque,
-            voluptas necessitatibus labore mollitia ratione, iusto veritatis sed
-            nesciunt aliquid, natus odit excepturi.
-          </Text>
-        </View>
-      </ShadowView>
+      <Commentary/>
+      <Commentary/>
+      <Commentary/>
     </ScrollView>
   );
 }
