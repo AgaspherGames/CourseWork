a791c650d91d679945a038e38a2eaf2b691c3594
AgaspherGames <agasphergg@gmail.com>
2023-10-19 18:10:52 +0600

    lik button animation works

--- components/UI/buttons/LikeButton.jsx
+++ components/UI/buttons/LikeButton.jsx
@@ -1,60 +1,42 @@
-import { View, Text, Animated, Pressable } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React, { useCallback, useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { twMerge } from "tailwind-merge";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  cond,
+} from "react-native-reanimated";
 
 export default function LikeButton() {
-  const scaleLike = new Animated.Value(0);
-  const scaleNoLike = new Animated.Value(1);
+
   const [isLiked, setIsLiked] = useState(false);
 
-  const handlePressIn = () => {
-    console.log("i");
-    setIsLiked(false);
-    Animated.spring(scaleLike, {
-      toValue: 0,
-      useNativeDriver: true,
-    }).start();
-    Animated.spring(scaleNoLike, {
-      toValue: 1,
-      useNativeDriver: true,
-    }).start();
-  };
+  const scale = useSharedValue(1);
 
-  const handlePressOut = () => {
-    console.log("o");
-    setIsLiked(true);
-    Animated.spring(scaleLike, {
-      toValue: 1,
-      useNativeDriver: true,
-    }).start();
-    Animated.spring(scaleNoLike, {
-      toValue: 0,
-      useNativeDriver: true,
-    }).start();
+  const likeStyles = useAnimatedStyle(() => ({
+    transform: [{ scale: 1-scale.value }],
+  }));
+  const noLikeStyles = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  const handlePress = () => {
+    setIsLiked((p) => !p);
+    scale.value = withSpring(isLiked ? 0 : 1);
   };
 
   return (
     <>
-      <Animated.View style={{ transform: [{ scale: scaleNoLike }] }}>
-        <Pressable
-          onPress={handlePressIn}
-          className=""
-        >
-          <AntDesign name={"heart"} size={24} />
-        </Pressable>
-      </Animated.View>
-      <Animated.View
-        className={twMerge("absolute", isLiked && "overflow-hidden")}
-        style={{ transform: [{ scale: scaleLike }] }}
-      >
-        <Pressable
-          onPress={handlePressOut}
-          className=""
-        >
-          <AntDesign name={"hearto"} size={24} />
-        </Pressable>
-      </Animated.View>
+      <Pressable onPress={handlePress} className="absolute">
+        <Animated.View style={[likeStyles]} >
+          <AntDesign name={"hearto"} size={24} color="red" />
+        </Animated.View>
+        <Animated.View className="absolute" style={[noLikeStyles]} >
+          <AntDesign name={"heart"} size={24} color="red" />
+        </Animated.View>
+      </Pressable>
     </>
   );
 }
